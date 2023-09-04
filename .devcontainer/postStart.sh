637024646986381930c571ff22972ddc95559903
DanielDKnudsen <daniel.d.knudsen@gmail.com>
2023-09-04 15:11:08 +0000

    feat: Working with functions

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,1 +1,6 @@
 echo "heyyyy"
+
+# pip install -r /workspaces/osrs/yolov8/requirements.txt
+cd /workspaces/osrs
+sudo chmod +x nuctl-1.11.24-linux-amd64
+sudo ln -sf $(pwd)/nuctl-1.11.24-linux-amd64 /usr/local/bin/nuctl
